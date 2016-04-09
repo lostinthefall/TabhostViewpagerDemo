5b98992bfc7816162b6c476d2e355a8a533a2a6e
lostinthefall <zhuangyinwu@gmail.com>
2016-04-09 13:25:26 +0800

    add like button without notes

--- app/src/main/java/com/atozmak/tabhostviewpagerdemo/frgm07/DotsView.java
+++ app/src/main/java/com/atozmak/tabhostviewpagerdemo/frgm07/DotsView.java
@@ -1,22 +1,203 @@
 package com.atozmak.tabhostviewpagerdemo.frgm07;
 
+import android.animation.ArgbEvaluator;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Property;
 import android.view.View;
 
 /**
+ * 发散的点, 由大点小点组成, 两类点排列和颜色均错开, 速度先慢后快向外发射.
+ * <p/>
  * Created by Mak on 2016/4/8.
  */
 public class DotsView extends View {
+
+    private static final int DOTS_COUNT = 7;
+    private static final int OUTER_DOTS_POSITION_ANGLE = 51;
+
+    private static final int COLOR_1 = 0xffffc107;
+    private static final int COLOR_2 = 0xffff9800;
+    private static final int COLOR_3 = 0xffff5722;
+    private static final int COLOR_4 = 0xfff44336;
+
+    private final Paint[] mCirclePaints = new Paint[4];
+
+    private int mCenterX;
+    private int mCenterY;
+
+    private float mMaxOuterDotsRadius;
+    private float mMaxInnerDotsRadius;
+    private float mMaxDotSize;
+
+    private float mCurrentProgress = 0;
+
+    private float mCurrentRadius1 = 0;
+    private float mCurrentDotSize1 = 0;
+    private float mCurrentRadius2 = 0;
+    private float mCurrentDotSize2 = 0;
+
+    private ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+
     public DotsView(Context context) {
         super(context);
+        init();
     }
 
     public DotsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public DotsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
+
+    private void init() {
+        for (int i = 0; i < mCirclePaints.length; i++) {
+            mCirclePaints[i] = new Paint();
+            mCirclePaints[i].setStyle(Paint.Style.FILL);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mCenterX = w / 2;
+        mCenterY = h / 2;
+        mMaxDotSize = 20;
+        mMaxOuterDotsRadius = w / 2 - mMaxDotSize * 2;
+        mMaxInnerDotsRadius = 0.8f * mMaxOuterDotsRadius;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawOuterDotsFrame(canvas);
+        drawInnerDotsFrame(canvas);
+    }
+
+    private void drawOuterDotsFrame(Canvas canvas) {
+        for (int i = 0; i < DOTS_COUNT; i++) {
+            int cX = (int) (mCenterX + mCurrentRadius1 * Math.cos(i * OUTER_DOTS_POSITION_ANGLE * Math.PI / 180));
+            int cY = (int) (mCenterX + mCurrentRadius1 * Math.sin(i * OUTER_DOTS_POSITION_ANGLE * Math.PI / 180));
+
+            canvas.drawCircle(cX, cY, mCurrentDotSize1, mCirclePaints[i % mCirclePaints.length]);
+        }
+
+
+    }
+
+    private void drawInnerDotsFrame(Canvas canvas) {
+        for (int i = 0; i < DOTS_COUNT; i++) {
+            int cX = (int) (mCenterX + mCurrentRadius2 * Math.cos((i * OUTER_DOTS_POSITION_ANGLE - 10) * Math.PI / 180));
+            int cY = (int) (mCenterY + mCurrentRadius2 * Math.sin((i * OUTER_DOTS_POSITION_ANGLE - 10) * Math.PI / 180));
+
+            //i+1 确保颜色不同
+            canvas.drawCircle(cX, cY, mCurrentDotSize2, mCirclePaints[(i + 1) % mCirclePaints.length]);
+        }
+    }
+
+    public void setCurrentProgress(float currentProgress) {
+        mCurrentProgress = currentProgress;
+
+        updateInnerDotsPosition();
+        updateOuterDotsPosition();
+
+        updateDotsPaints();
+        updateDotsAlpha();
+
+        postInvalidate();
+    }
+
+
+    public float getCurrentProgress() {
+        return mCurrentProgress;
+    }
+
+
+    private void updateDotsAlpha() {
+        float progress = (float) ProAnimUtils.clamp(mCurrentProgress, 0.6f, 1f); // 最小0.6, 最大1
+        int alpha = (int) ProAnimUtils.mapValueFromRangeToRange(progress, 0.6f, 1f, 255, 0); // 直至消失
+        mCirclePaints[0].setAlpha(alpha);
+        mCirclePaints[1].setAlpha(alpha);
+        mCirclePaints[2].setAlpha(alpha);
+        mCirclePaints[3].setAlpha(alpha);
+
+
+    }
+
+    private void updateDotsPaints() {
+
+        if (mCurrentProgress < 0.5f) {
+            float progress = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0f, 0.5f, 0, 1f);
+            mCirclePaints[0].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_1, COLOR_2));
+            mCirclePaints[1].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_2, COLOR_3));
+            mCirclePaints[2].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_3, COLOR_4));
+            mCirclePaints[3].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_4, COLOR_1));
+        } else {
+            float progress = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.5f, 1f, 0, 1f);
+            mCirclePaints[0].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_2, COLOR_3));
+            mCirclePaints[1].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_3, COLOR_4));
+            mCirclePaints[2].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_4, COLOR_1));
+            mCirclePaints[3].setColor((Integer) argbEvaluator.evaluate(progress, COLOR_1, COLOR_2));
+        }
+
+
+    }
+
+    private void updateOuterDotsPosition() {
+        // 半径先走的快, 后走的慢
+        if (mCurrentProgress < 0.3f) {
+            mCurrentRadius1 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.0f, 0.3f, 0, mMaxOuterDotsRadius * 0.8f);
+        } else {
+            mCurrentRadius1 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.3f, 1f, 0.8f * mMaxOuterDotsRadius, mMaxOuterDotsRadius);
+        }
+
+        // 点的大小, 小于0.7是最大点, 大于0.7逐渐为0.
+        if (mCurrentProgress < 0.7f) {
+            mCurrentDotSize1 = mMaxDotSize;
+        } else {
+            mCurrentDotSize1 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.7f, 1f, mMaxDotSize, 0);
+        }
+
+
+    }
+
+    private void updateInnerDotsPosition() {
+
+        //0.3以上不动
+        if (mCurrentProgress < 0.3f) {
+            this.mCurrentRadius2 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0, 0.3f, 0.0f, mMaxInnerDotsRadius);
+        } else {
+            this.mCurrentRadius2 = mMaxInnerDotsRadius;
+        }
+
+        if (mCurrentProgress < 0.2) {
+            this.mCurrentDotSize2 = mMaxDotSize;
+        } else if (mCurrentProgress < 0.5) {
+            this.mCurrentDotSize2 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.2f, 0.5f, mMaxDotSize, 0.3 * mMaxDotSize);
+        } else {
+            this.mCurrentDotSize2 = (float) ProAnimUtils.mapValueFromRangeToRange(mCurrentProgress, 0.5f, 1f, mMaxDotSize * 0.3f, 0);
+        }
+
+    }
+
+    public static final Property<DotsView, Float> DOTS_PROGRESS
+            = new Property<DotsView, Float>(Float.class, "dotsProgress") {
+        @Override
+        public Float get(DotsView object) {
+            return object.getCurrentProgress();
+        }
+
+        @Override
+        public void set(DotsView object, Float value) {
+            object.setCurrentProgress(value);
+        }
+    };
+
+
 }
--- app/src/main/java/com/atozmak/tabhostviewpagerdemo/frgm07/LikeButtonView.java
+++ app/src/main/java/com/atozmak/tabhostviewpagerdemo/frgm07/LikeButtonView.java
@@ -7,6 +7,7 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
@@ -48,17 +49,21 @@ public class LikeButtonView extends FrameLayout implements View.OnClickListener 
         init();
     }
 
+    public LikeButtonView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+        mCvCircle = (CircleView) findViewById(R.id.like_button_cv_circle);
+        mIvStar = (ImageView) findViewById(R.id.like_button_iv_star);
+        mDvDots = (DotsView) findViewById(R.id.like_button_dv_dots);
     }
 
     private void init() {
 
-        mCvCircle = (CircleView) findViewById(R.id.like_button_cv_circle);
-        mIvStar = (ImageView) findViewById(R.id.like_button_iv_star);
-        mDvDots = (DotsView) findViewById(R.id.like_button_dv_dots);
 
         //不懂；
         isInEditMode();
@@ -84,8 +89,12 @@ public class LikeButtonView extends FrameLayout implements View.OnClickListener 
 
         if (mIsChecked) {
 
+            mIvStar.animate().cancel();
+            mIvStar.setScaleX(0);
+            mIvStar.setScaleY(0);
             mCvCircle.setInnerCircleRadiusProgress(0);
             mCvCircle.setOuterCircleRadiusProgress(0);
+            mDvDots.setCurrentProgress(0);
 
             mAnimatorSet = new AnimatorSet();
 
@@ -107,20 +116,41 @@ public class LikeButtonView extends FrameLayout implements View.OnClickListener 
             innerCircleAnimator.setStartDelay(200);
             innerCircleAnimator.setInterpolator(mDecelerateInterpolator);
 
+            ObjectAnimator starScaleYAnimator = ObjectAnimator.ofFloat(mIvStar, ImageView.SCALE_Y, 0.2f, 1f);
+            starScaleYAnimator.setDuration(350);
+            starScaleYAnimator.setStartDelay(250);
+            starScaleYAnimator.setInterpolator(mOvershootInterpolator);
+
+            ObjectAnimator starScaleXAnimator = ObjectAnimator.ofFloat(mIvStar, ImageView.SCALE_X, 0.2f, 1f);
+            starScaleXAnimator.setDuration(350);
+            starScaleXAnimator.setStartDelay(250);
+            starScaleXAnimator.setInterpolator(mOvershootInterpolator);
+
+            ObjectAnimator dotsAnimator = ObjectAnimator.ofFloat(mDvDots, DotsView.DOTS_PROGRESS, 0, 1f);
+            dotsAnimator.setDuration(900);
+            dotsAnimator.setStartDelay(50);
+            dotsAnimator.setInterpolator(mAccelerateDecelerateInterpolator);
+
 
             mAnimatorSet.playTogether(
                     outerCircleAnimator,
-                    innerCircleAnimator
-
-
+                    innerCircleAnimator,
+                    starScaleYAnimator,
+                    starScaleXAnimator,
+                    dotsAnimator
             );
 
 
             mAnimatorSet.addListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationCancel(Animator animation) {
+                    mIvStar.setScaleX(1);
+                    mIvStar.setScaleY(1);
+
                     mCvCircle.setOuterCircleRadiusProgress(0);
                     mCvCircle.setInnerCircleRadiusProgress(0);
+
+                    mDvDots.setCurrentProgress(0);
                 }
             });
 
@@ -131,5 +161,32 @@ public class LikeButtonView extends FrameLayout implements View.OnClickListener 
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mIvStar.animate().scaleX(0.7f).scaleY(0.7f).setDuration(150).setInterpolator(mDecelerateInterpolator);
+                setPressed(true);
+                break;
 
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                float y = event.getY();
+                boolean isInside = (x > 0 && x < getWidth() && y > 0 && y < getHeight());
+                if (isPressed() != isInside) {
+                    setPressed(isInside);
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                mIvStar.animate().scaleX(1).scaleY(1).setInterpolator(mDecelerateInterpolator);
+                if (isPressed()) {
+                    performClick();
+                    setPressed(false);
+                }
+                break;
+        }
+        return true;
+    }
 }
+
